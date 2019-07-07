7239ebd80bbd4b37f0ed8bd2d1531860cae19120
Gabriel Moterani <contatomoterani@gmail.com>
2019-07-06 22:37:59 -0300

    like component

---
+++ src/components/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Like extends Component {
+    render() { 
+        let classes = "fa fa-heart"
+        if(this.props.liked === true) classes +=  "-o";
+        return (<i className={classes}></i>);
+    }
+}
+ 
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import {getMovies, deleteMovie} from '../services/fakeMovieService'
+import {getMovies} from '../services/fakeMovieService';
+import Like from './like'
 class MoviesComponent extends Component {
     state = { 
         movies: getMovies()
      }
+
+     handleLike = (id) => {
+        const movies = [...this.state.movies];
+        const indexOfChanged = movies.map(m => m._id).indexOf(id);
+        movies[indexOfChanged].liked = !movies[indexOfChanged].liked
+        this.setState({movies});
+     }
+     
     render() { 
         const count = this.state.movies.length;
         if( count === 0 ) return <p>No Movies to Show</p>
@@ -17,6 +26,7 @@ class MoviesComponent extends Component {
                         <th scope="col">Genre</th>
                         <th scope="col">Stock</th>
                         <th scope="col">Rate</th>
+                        <th scope="col">Like</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,6 +36,7 @@ class MoviesComponent extends Component {
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td><a onClick={() => this.handleLike(movie._id)}><Like  liked={movie.liked}/></a></td>
                                 <td><button onClick={() => this.handleDeleteMovieInServer(movie)} className="btn btn-danger">Delete</button></td>
                             </tr>
                         )}
